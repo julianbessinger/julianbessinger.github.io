b5f73fc4df947836e6c2919e4d6e5ac978dd4681
julianbessinger <57990318+julianbessinger@users.noreply.github.com>
2020-03-21 12:05:34 +0100

    Add files via upload
    Tip Calculator outline

---
+++ js/tipCalculator.js
@@ -0,0 +1,9 @@
+1. /*Create a form on a blank HTML page (with my pages nav bar and styling, etc)
+2. Ask for bill total, 
+3. quality of service (15, 18, 20, 22)
+4. Number of people splitting the bill
+5. Multiply the bill total by .18, .2 or .22
+6. Divide this number by the total number of people paying. 
+7. Create a submit button, then have the calculation run onClick for that.
+8. Style the form.
+9. Make a screenshot of the app, then store it in the portfolio page of the main website.*/
